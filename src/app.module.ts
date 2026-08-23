f0202548b4c338ad82835b6016463446715c4d1f
Ayush Bommana <bommanaayush07@gmail.com>
2026-08-24 00:15:55 +0530

    feat: setup the config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import serverConfig from './config/server.config';
+import databaseConfig from './config/database.config';
+import envsValidationSchema from './config/validations/envs.validation';
+
+const NODE_ENV = process.env.NODE_ENV ?? 'development';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: envsValidationSchema,
+      load: [serverConfig, databaseConfig],
+      envFilePath: `.env.${NODE_ENV}`,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
